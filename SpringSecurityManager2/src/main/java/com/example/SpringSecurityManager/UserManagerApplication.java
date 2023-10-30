c96fb1e65d1c3e6a348e2f5d7c12b4bc6f1642b7
Sravani-1202 <noorasravani1202@gmail.com>
2023-10-30 16:09:54 +0530

    TwentyThirdCommit

---
+++ SpringSecurityManager2/src/main/java/com/example/SpringSecurityManager/UserManagerApplication.java
@@ -0,0 +1,17 @@
+package com.example.SpringSecurityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+public class UserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerApplication.class, args);
+		System.out.println("started...");
+	}
+
+}
---
+++ SpringSecurityManager2/src/main/java/com/example/SpringSecurityManager/bindings/Users.java
@@ -0,0 +1,7 @@
+package com.example.SpringSecurityManager.bindings;
+
+public class Users {
+private String Name;
+ private Integer id;
+ 
+}
